ff4aa85d352e968f7ea46b7b510ef1ea62181384
rishowrav <rishowrav@gmail.com>
2024-08-02 23:25:37 +0600

    asc & desc search condition added and min & max price search functionality added

--- index.js
+++ index.js
@@ -1,9 +1,74 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// middleware
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+// MongoDB start
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sp25joa.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const allProducts = client.db("mongoDB_Operation").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const filter = req.query;
+      console.log(filter);
+      const query = {
+        price: {
+          $gte: parseInt(filter.min) || 0,
+          $lte: parseInt(filter.max) || 10000,
+        },
+      };
+      const options = {
+        sort: {
+          price: filter.sort !== "asc" ? -1 : 1,
+        },
+      };
+
+      const result = await allProducts.find(query, options).toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// MongoDB End
 
 app.get("/", (req, res) => {
-  res.send("Hello World! Hello Shworav");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
