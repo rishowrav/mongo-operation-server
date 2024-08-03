2c2a58cf9f831d7574c346c473f6e325adddd82e
rishowrav <rishowrav@gmail.com>
2024-08-03 23:08:13 +0600

    fixed cors error

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://mongo-operation.netlify.app"],
     credentials: true,
   })
 );
